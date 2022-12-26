b880c0fe4ddca72abd2362b551edfdb80e98f68e
Jacob S <jacobraysickels@gmail.com>
2022-12-26 17:02:25 -0600

    2022 Day 24 Part 1

--- src/advent_of_code/2022/day_24.clj
+++ src/advent_of_code/2022/day_24.clj
@@ -4,7 +4,7 @@
             [advent-of-code.shared.point :as points]
             [clojure.set :as set]))
 
-(def data (->> (read/read-file "resources/2022/day_24.txt")
+(def data (->> (read/read-file "resources/2022/day_24_example.txt")
                (map-indexed (fn [i line]
                               (map-indexed
                                 (fn [j space] (list [j i] space))
@@ -15,68 +15,74 @@
 (def walls (->> (filter #(= "#" (second %)) data)
                 (map first)
                 set
-                (set/union #{[1 -1] [6 6]}))) ;; Change these for real data
+                (set/union #{[1 -1] [6 6]})))               ;; Change these for real data
 
 (def tornadoes (->> (remove #(or (= "." (second %)) (= "#" (second %))) data)))
 
-(defn next-tornado-position [tornado area-width area-height]
+(defn next-tornado-position [tornado min-x max-x min-y max-y]
   (let [[[x y] direction] tornado]
     [(cond (= direction ">")
-           (if (<  area-width (inc x))
-             [1 y]
+           (if (< max-x (inc x))
+             [min-x y]
              [(inc x) y])
 
            (= direction "<")
-           (if (= 0 (dec x))
-             [area-width y]
+           (if (> min-x (dec x))
+             [max-x y]
              [(dec x) y])
 
            (= direction "^")
-           (if (= 0 (dec y))
-             [x  area-height]
+           (if (> min-y (dec y))
+             [x max-y]
              [x (dec y)])
 
-           :else (if (< area-height (inc y))
-                   [x 1]
+           :else (if (< max-y (inc y))
+                   [x min-y]
                    [x (inc y)])) direction]))
 
-(defn move-tornadoes [tornadoes area-width area-height]
-  (map #(next-tornado-position % area-width area-height) tornadoes))
+(defn move-tornadoes [tornadoes min-x max-x min-y max-y]
+  (map #(next-tornado-position % min-x max-x min-y max-y) tornadoes))
 
-(defn next-moves [position walls tornadoes]
-  (println "position" position)
+(defn next-moves [position walls tornadoes start-position]
   (let [around (points/cardinal-points-around position)]
-    (-> (set/difference (set around) walls)
+    (-> (set/difference (set around) #{start-position})
+        (set/difference walls)
         (set/difference (set (map first tornadoes))))))
 
-(defn next-positions [positions walls tornadoes]
-  (set (mapcat #(next-moves % walls tornadoes) positions)))
-
-(defn should-stop? [lines end-position]
-  (or
-    (->> (map count lines)
-         (filter #(> % 18))
-         (empty?)
-         not)
-    (->> (map last lines)
-         (filter #(= end-position %))
-         (empty?)
-         not)))
+(defn next-positions [positions walls tornadoes start-position]
+  (set (mapcat #(let [n-moves (next-moves % walls tornadoes start-position)]
+                  (if (empty? n-moves)
+                    #{%}
+                    n-moves))
+               positions)))
 
 (defn move-to-end [start-position end-position walls tornadoes]
   (loop [t tornadoes
          positions #{start-position}
          steps 0]
-    (if (contains? positions end-position)
-      steps
-      (let [next-tornadoes (move-tornadoes t 6 4)
-            new-positions (next-positions positions walls next-tornadoes)]
-        (if (empty? new-positions)
-          (recur next-tornadoes
-                 positions
-                 (inc steps))
-          (recur next-tornadoes
-                 (next-positions positions walls next-tornadoes)
-                 (inc steps)))))))
+    ;(println positions steps)
+    (cond
+      (contains? positions end-position)
+      [steps t]
+
+      :else (let [_ "spacing"
+                  [min-x max-x] [1 6]
+                  [min-y max-y] [1 4]
+                  ;[min-x max-x] [1 120]
+                  ;[min-y max-y] [1 25]
+                  next-tornadoes (move-tornadoes t min-x max-x min-y max-y)
+                  new-positions (next-positions positions walls next-tornadoes start-position)]
+              (recur next-tornadoes
+                     new-positions
+                     (inc steps))))))
+
+(defn walk-across [start-pos end-pos walls tornadoes]
+  (let [[first-across first-tornadoes] (move-to-end start-pos end-pos walls tornadoes)
+        _ (println "done first")
+        [second-across second-tornadoes] (move-to-end end-pos start-pos walls first-tornadoes)
+        _ (println "done second")
+        [third-across _] (move-to-end start-pos end-pos walls second-tornadoes)
+        _ (println "done third")]
+    [first-across second-across third-across]))
 
 
