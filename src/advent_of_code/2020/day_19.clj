e4eb20d024283143e143c803b524502034121149
Jacob S. <jacobraysickels@gmail.com>
2023-12-19 21:13:31 -0600

    2020 Day 19

--- src/advent_of_code/2020/day_19.clj
+++ src/advent_of_code/2020/day_19.clj
@@ -2,59 +2,89 @@
   (:require [clojure.string :as str]
             [advent-of-code.shared.read-file :as read]))
 
-(def test-rules ["0: 4 1 5"
-                 "1: 2 3 | 3 2"
-                 "2: 4 4 | 5 5"
-                 "3: 4 5 | 5 4"
-                 "4: \"a\""
-                 "5: \"b\""])
+(def test-rules (->> (read/read-file "resources/2020/2020-19-example-rules.txt")
+                     (map #(let [[k v] (str/split % #": ")]
+                             (if (re-find #"[ab]" v)
+                               {k {:value (first (re-seq #"[ab]" v))}}
+                               {k {:value nil :children (->> (str/split v #" | ")
+                                                             (partition-by (fn [c] (= "|" c)))
+                                                             (remove (fn [col] (= (first col) "|"))))}})))
+                     (apply merge)))
+
+(def rules (->> (read/read-file "resources/2020/day-19-rules.txt")
+                (map #(let [[k v] (str/split % #": ")]
+                        (if (re-find #"[ab]" v)
+                          {k {:value (first (re-seq #"[ab]" v))}}
+                          {k {:value nil :children (->> (str/split v #" | ")
+                                                        (partition-by (fn [c] (= "|" c)))
+                                                        (remove (fn [col] (= (first col) "|"))))}})))
+                (apply merge)))
+(def test-data (read/read-file "resources/2020/2020-19-example-data.txt"))
+(def data (read/read-file "resources/2020/day-19-data.txt"))
 
 
-(defn format-data [input]
-  (apply merge (map #(let [[k v] (str/split % #": ")]
-                       (if (re-find #"[ab]" v)
-                         {k {:value (first (re-seq #"[ab]" v))}}
-                         {k {:value nil :regex (str/replace v #" " "") :deps (set (re-seq #"\d+" v))}}))
-                    input)))
-
-(def formatted (format-data (read/read-file "resources/2020/day-19-rules.txt")))
-;(def formatted (format-data test-rules))
-
-(defn replace-regex-with-deps-value [k formatted]
-  (let [replace-pattern (reduce (fn [acc item] (assoc acc item (:value (get formatted item)))) {} (:deps (get formatted k)))]
-    {k (merge (get formatted k)
-              {:value (str "("
-                           (str/replace (:regex (get formatted k))
-                                        (re-pattern (str/join "|" (:deps (get formatted k))))
-                                        replace-pattern)
-                           ")")})}))
-
-(defn deps-have-value? [k formatted]
-  (let [deps (:deps (get formatted k))]
-    (if (nil? deps)
-      false
-      (->> (map #(:value (get formatted %)) deps)
-           (every? #(not (nil? %)))))))
-
-(defn make-regex-for-deps [formatted]
-  (reduce (fn [acc [k v]]
-            (if (deps-have-value? k formatted)
-              (merge acc (replace-regex-with-deps-value k formatted))
-              (assoc acc k v)))
-          {}
-          formatted))
-
-(defn iterate-until-zero-has-value []
-  (loop [rules formatted]
-    (if (:value (get rules "0"))
-      rules
-      (recur (make-regex-for-deps rules)))))
+(defn build-regex [rules key]
+  (let [rule (get rules key)]
+    (if (contains? #{"a" "b"} (:value rule))
+      (:value rule)
+      (str "("
+           (->> (map (fn [col]
+                       (->> (map (fn [c] (build-regex rules c)) col)
+                            (str/join "")))
+                     (:children rule))
+                (str/join "|"))
+           ")"))))
 
 
 (defn part-1 []
-  (let [rules (iterate-until-zero-has-value)
-        check-regex (get-in rules ["0" :value])
-        to-check (read/read-file "resources/2020/day-19-data.txt")]
-    (->> (map #(list % (first (re-matches (re-pattern check-regex) %))) to-check)
+  (let [regex (build-regex rules "0")]
+    (->> (map #(list % (first (re-matches (re-pattern regex) %))) data)
+         (filter (fn [[f s]] (= f s)))
+         count)))
+
+(defn part-1-test []
+  (let [regex (build-regex test-rules "0")]
+    (->> (map #(list % (first (re-matches (re-pattern regex) %))) test-data)
+         (filter (fn [[f s]] (= f s)))
+         count)))
+
+(declare memoized-build-regex-2)
+
+(defn build-regex-2 [rules key]
+  (cond
+    (= key "8")
+    (str (memoized-build-regex-2 rules "42") "+")
+
+    (= key "11")
+    (let [fourtytwo (memoized-build-regex-2 rules "42")
+          thirtyone (memoized-build-regex-2 rules "31")]
+      (str "("
+           (->> (map #(str "(" fourtytwo "{" % "}" thirtyone "{" % "}" ")") (range 1 4))
+                (str/join "|"))
+           ")"))
+
+    :else (let [rule (get rules key)]
+            (if (contains? #{"a" "b"} (:value rule))
+              (:value rule)
+              (str "("
+                   (->> (map (fn [col]
+                               (->> (map (fn [c] (memoized-build-regex-2 rules c)) col)
+                                    (str/join "")))
+                             (:children rule))
+                        (str/join "|"))
+                   ")")))))
+
+(def memoized-build-regex-2 (memoize build-regex-2))
+
+(defn part-2-test []
+  "must replace rules in build-regex-2 with test-rules"
+  (let [regex (build-regex-2 test-rules "0")]
+    (->> (map #(list % (first (re-matches (re-pattern regex) %))) test-data)
+         (filter (fn [[f s]] (= f s)))
+         count)))
+
+(defn part-2 []
+  (let [regex (build-regex-2 rules "0")]
+    (->> (map #(list % (first (re-matches (re-pattern regex) %))) data)
          (filter (fn [[f s]] (= f s)))
          count)))
