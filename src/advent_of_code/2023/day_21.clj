bd61446bc0102826b55303839c12ee41b8a6cc1f
Jacob S. <jacobraysickels@gmail.com>
2023-12-22 01:42:10 -0600

    2023 Day 22 Part 1

---
+++ src/advent_of_code/2023/day_21.clj
@@ -0,0 +1,78 @@
+(ns advent-of-code.2023.day-21
+  (:require [advent-of-code.shared.point :as points]
+            [advent-of-code.shared.read-file :as read]
+            [clojure.math.combinatorics :as combo]
+            [clojure.set :as set]
+            [clojure.string :as str]))
+
+(def data (read/read-file "resources/2023/day_21.txt"))
+
+(def starting-point (let [[y col] (->> (map-indexed (fn [i col] (if (str/includes? col "S") [i col] [i nil])) data)
+                                       (filter #(not (nil? (second %))))
+                                       first)]
+                      [(str/index-of col "S") y]))
+
+(def finish-point (let [[y col] (->> (map-indexed (fn [i col] (if (str/includes? col "S") [i col] [i nil])) data)
+                                     (filter #(not (nil? (second %))))
+                                     first)]
+                    [(str/index-of col "F") y]))
+
+(def walls
+  (->> data
+       (map-indexed (fn [row line]
+                      (map-indexed (fn [column character] (if (= \# character) [column row] nil))
+                                   line)))
+       (apply concat)
+       (remove nil?)
+       set))
+
+(defn part-1 []
+  (loop [points #{starting-point}
+         steps 0]
+    (if (= steps 64)
+      points
+      (let [next-steps (set/difference (set (mapcat points/cardinal-points-around points)) walls)]
+        (recur next-steps (inc steps))))))
+
+(declare memoized-is-point-wall?)
+
+(defn is-point-wall? [[x y _]]
+  (let [width (count (first data))
+        height (count data)]
+    (= \# (get-in data (reverse [(mod x width) (mod y height)])))))
+
+(def memoized-is-point-wall? (memoize is-point-wall?))
+
+(defn collapse-points [points]
+  (let [width (count (first data))
+        height (count data)]
+    (->> (map (fn [[x y c]] [(mod x width) (mod y height) c]) points)
+         (sort-by (fn [[x y c]] [x y]))
+         (partition-by (fn [[x y c]] [x y]))
+         (map (fn [col] (conj (vec (take 2 (first col))) (->> (map last col)
+                                                              (reduce +))))))))
+
+
+(defn remove-duplicates [points]
+  (loop [p points
+         visited #{}
+         result []]
+    (if (empty? p)
+      result
+      (if (contains? visited (take 2 (first p)))
+        (recur (rest p) visited result)
+        (recur (rest p) (conj visited (take 2 (first p))) (conj result (first p)))))))
+
+
+(defn part-2 []
+  (loop [points [(conj starting-point 1)]
+         steps 0]
+    ;(println points)
+    (if (= steps 10)
+      points
+      (let [next-steps (->> points
+                            (mapcat (fn [[x y amt]] (map (fn [point] (conj point amt)) (points/cardinal-points-around [x y]))))
+                            (filter (fn [[x y amt]] (not (is-point-wall? [x y]))))
+                            remove-duplicates
+                            collapse-points)]
+        (recur next-steps (inc steps))))))
---
+++ src/advent_of_code/2023/day_22.clj
@@ -0,0 +1,94 @@
+(ns advent-of-code.2023.day-22
+  (:require [advent-of-code.shared.point :as points]
+            [advent-of-code.shared.read-file :as read]
+            [advent-of-code.shared.utils :as utils]
+            [clojure.set :as set]
+            [clojure.string :as str]))
+
+
+(def data (->> (read/read-file "resources/2023/day_22.txt")
+               (map #(let [[f s] (str/split % #"~")
+                           [x1 y1 z1] (map (fn [c] (Integer/parseInt c)) (str/split f #","))
+                           [x2 y2 z2] (map (fn [c] (Integer/parseInt c)) (str/split s #","))]
+                       [[x1 y1 z1] [x2 y2 z2]]))))
+
+(defn points-between-3d [block]
+  (let [[[x1 y1 z1] [x2 y2 z2]] block]
+    (cond
+      (and (= x1 x2) (= y1 y2))
+      (map #(vec [x1 y1 %]) (apply utils/inclusive-range (sort [z1 z2])))
+
+      (and (= x1 x2) (= z1 z2))
+      (map #(vec [x1 % z1]) (apply utils/inclusive-range (sort [y1 y2])))
+
+      (and (= y1 y2) (= z1 z2))
+      (map #(vec [% y1 z1]) (apply utils/inclusive-range (sort [x1 x2]))))))
+
+(defn get-block-points [block]
+  (points-between-3d block))
+
+(def memoized-get-block-points (memoize get-block-points))
+
+(defn descended-block-points [block return-block?]
+  (let [block-points (mapv (fn [[x y z]] [x y (dec z)]) (memoized-get-block-points block))]
+    (if return-block? [(first block-points) (last block-points)] block-points)))
+
+(defn get-block-height [block]
+  (apply min (map last block)))
+
+(defn is-block-rested? [block rested-blocks]
+  (let [check-descended (descended-block-points block false)
+        rested-block-points (set (mapcat #(memoized-get-block-points %) rested-blocks))]
+    (cond
+      (not (empty? (set/intersection rested-block-points (set check-descended))))
+      true
+
+      :else (zero? (get-block-height check-descended)))))
+
+(defn blocks-to-rest [blocks]
+  (loop [col (sort-by get-block-height blocks)
+         check-block (first col)
+         rested #{}]
+    (println (count col))
+    (if (empty? col)
+      rested
+      (let [is-rested? (is-block-rested? check-block rested)]
+        (cond
+          is-rested?
+          (recur (rest col)
+                 (first (rest col))
+                 (conj rested check-block))
+
+
+
+          :else (recur col
+                       (descended-block-points check-block true)
+                       rested))))))
+
+(defn find-support [block rested-blocks]
+  (let [block-points (descended-block-points block false)
+        check-blocks (disj rested-blocks block)
+        support-blocks (->> check-blocks
+                            (filter #(not (empty? (set/intersection (set (memoized-get-block-points %)) (set block-points))))))]
+    (if (empty? support-blocks) :ground support-blocks)))
+
+(defn find-supports [rested-blocks]
+  (map #(array-map :block % :supports (find-support % rested-blocks)) rested-blocks))
+
+(defn can-block-be-removed? [block rested-block-supports]
+  (->>
+    (remove #(= block (:block %)) rested-block-supports)
+    (map :supports)
+    (map #(if (= :ground %)
+            [:ground]
+            (remove (fn [support-block] (= support-block block)) %)))
+    (filter empty?)
+    empty?))
+
+(defn part-1 []
+  (let [rested-blocks (blocks-to-rest data)
+        _ (println "rested")
+        supported-blocks (find-supports rested-blocks)]
+    (println "supports")
+    (frequencies (map #(can-block-be-removed? (:block %) supported-blocks) supported-blocks))))
+
