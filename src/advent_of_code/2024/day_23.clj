139fb0bc3f706dd28cbd98d70f56d00347d06126
Jacob S. <jacobraysickels@gmail.com>
2024-12-23 17:20:13 -0600

    2024 Day 23 cleanup

--- src/advent_of_code/2024/day_23.clj
+++ src/advent_of_code/2024/day_23.clj
@@ -1,9 +1,8 @@
 (ns advent-of-code.2024.day-23
   (:require [advent-of-code.shared.read-file :as read]
+            [clojure.math.combinatorics :as combo]
             [clojure.set :as set]
             [clojure.string :as str]))
-
-(require '[clojure.data.priority-map :refer [priority-map]])
 
 (def data (->> (read/read-file "resources/2024/day_23.txt")
                (map #(str/split % #"-"))))
@@ -14,20 +13,11 @@
     (if (empty? connections)
       acc
       (let [connection (first connections)]
-        (recur (rest connections) (assoc acc (first connection) (conj (get acc (first connection) #{}) (second connection))
-                                             (second connection) (conj (get acc (second connection) #{}) (first connection))))))))
+        (recur (rest connections)
+               (assoc acc (first connection) (conj (get acc (first connection) #{}) (second connection))
+                          (second connection) (conj (get acc (second connection) #{}) (first connection))))))))
 
-(defn generate-first-permutations [computers lan-map]
-  (let [unique-computers (set (keys lan-map))]
-    (->> (set/difference unique-computers computers)
-         (map #(conj computers %)))))
-
-(defn generate-second-permutations [computers lan-map]
-  (let [unique-computers (set (keys lan-map))
-        perms (generate-first-permutations computers lan-map)]
-    (set (mapcat #(->> (set/difference unique-computers %)
-                       (map (fn [c] (conj % c)))) perms))))
-
+(def lan-map (get-lan-map))
 
 (defn are-computers-connected? [computers lan-map]
   (every? #(set/subset? (set/difference computers #{%}) (get lan-map %)) computers))
@@ -36,33 +26,27 @@
   (->> (filter #(str/starts-with? % "t") connections)
        (not-empty)))
 
+(defn permutations []
+  (->> (mapcat (fn [[k v]] (combo/cartesian-product #{k} v)) lan-map)
+       (mapcat (fn [[k v]] (map #(conj % k) (combo/cartesian-product #{v} (get lan-map v)))))
+       (map set)
+       set
+       (filter #(= (count %) 3))))
 
 (defn part-1 []
-  (let [lan-map (get-lan-map)
-        unique-computers (set (keys lan-map))
-        check-connections (set (mapcat #(generate-second-permutations #{%} lan-map) unique-computers))]
-    (->> (filter #(are-computers-connected? % lan-map) check-connections)
+  (let [perms (permutations)]
+    (->> (filter #(are-computers-connected? % lan-map) perms)
          (filter connection-has-t?)
          (count))))
 
-;; =====
-(defn get-lan-map-2 []
-  (loop [connections data
-         acc {}]
-    (if (empty? connections)
-      acc
-      (let [connection (first connections)]
-        (recur (rest connections) (assoc acc (first connection) (assoc (get acc (first connection)) (second connection) 1)
-                                             (second connection) (assoc (get acc (second connection)) (first connection) 1)))))))
-
-(defn get-groups [check lan-map]
-  (loop [v (keys (get lan-map check))
+(defn get-groups [check]
+  (loop [v (get lan-map check)
          groups [#{}]]
     (if (empty? v)
       (->> (map #(conj % check) groups)
            (sort-by count)
            (last))
-      (let [next-level (keys (get lan-map (first v)))]
+      (let [next-level (get lan-map (first v))]
         (recur
           (rest v)
           (reduce (fn [acc g]
@@ -79,9 +63,8 @@
                   groups))))))
 
 (defn part-2 []
-  (let [lan-map (get-lan-map-2)
-        unique-computers (set (keys lan-map))]
-    (->> (map #(get-groups % lan-map) unique-computers)
+  (let [unique-computers (set (keys lan-map))]
+    (->> (map #(get-groups %) unique-computers)
          (sort-by count)
          (partition-by count)
          (last)
